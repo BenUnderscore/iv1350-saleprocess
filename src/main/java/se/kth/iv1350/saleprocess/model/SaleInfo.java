aa7f4e5a3997f0ab7016bda6e3d7e61a84f4bef1
DjFluff <85769588+DjFluff@users.noreply.github.com>
2024-04-30 16:27:49 +0200

    Update SaleInfo.java
    i dunno is half finished, gotta go. uwawawawawa

--- src/main/java/se/kth/iv1350/saleprocess/model/SaleInfo.java
+++ src/main/java/se/kth/iv1350/saleprocess/model/SaleInfo.java
@@ -1,38 +1,43 @@
 package se.kth.iv1350.saleprocess.model;
 import java.util.ArrayList;
+import java.time.LocalDateTime;
+import se.kth.iv1350.saleprocess.integrations.DiscountRegistryHandler;
+import se.kth.iv1350.saleprocess.dto.ItemInfo;
 
 public class SaleInfo {
-    ArrayList<ItemInfo> itemList;
-    int totalPrice;
-    int currentTotal;
+    public ArrayList<ItemInfo> itemList;
+    public int totalPrice;
+    public int currentTotal;
+    public int discountAmount; 
+    public LocalDateTime currentDate; 
 
     public SaleInfo(ArrayList<ItemInfo> itemList, int totalPrice ){
         this.itemList = itemList;
         this.totalPrice = totalPrice;
     }
 
-    public int getTotalPrice() {
+    public ArrayList<ItemInfo> getItemList(){
+        return itemList;
+    }
 
+    public int getTotalPrice() {
         return totalPrice;
     }
 
     public int getCurrentTotal() {
-
         return currentTotal;
     }
 
     public void createReceipt(){
+        this.currentDate = LocalDateTime.now();
+    }
+
+    public void applyDiscount(DiscountRegistryHandler DiscountRegistryHandler, String customerID){
+        this.discountAmount = DiscountRegistryHandler.getDiscount(customerID, totalPrice, itemList);
+        currentTotal -= discountAmount;
+    } 
+
+    public Receipt createReceipt(){
         
     }
-
-    public ArrayList<ItemInfo> getItemList(){
-
-    }
-
-    public void applyDiscount(){
-
-    }
-
-    
-
 }
