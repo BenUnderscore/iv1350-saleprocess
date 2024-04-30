736af2539427c0b3e72f326cc5671e8a16e329b5
DjFluff <dante.j.cosentino@hotmail.com>
2024-04-30 12:36:10 +0200

    made receipt DTO
    see title,

--- src/main/java/se/kth/iv1350/saleprocess/dto/Receipt.java
+++ src/main/java/se/kth/iv1350/saleprocess/dto/Receipt.java
@@ -1,13 +1,35 @@
 package se.kth.iv1350.saleprocess.dto;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+
 class Receipt {
 
+    int initialPrice;
+    int finalPrice;
+    int amountPaid;
+    int change;
+    Date date;
+    ArrayList<ItemInfo> itemList;
+
+    /**
+     * Creates receipt DTO, with the below params
+     * @param initialPrice price of sale before any discounts are applied
+     * @param finalPrice price of sale after relevant discounts are applied, 
+     * @param amountPaid the amount paid by the customer
+     * @param change the change returned to the customer after paying 
+     * @param itemList a list of all items that have been in the purchase
+     */
+    public Receipt(int initialPrice, int finalPrice, int amountPaid, int change, ArrayList<ItemInfo> itemList){
+        this.initialPrice = initialPrice;
+        this.finalPrice = finalPrice;
+        this.amountPaid = amountPaid;
+        this.change = change;
+        this.itemList = itemList;
+        this.date = new Date();
+
+    }
 
 
-}
-public Class Receipt {
-    String name;
-    StReceipt
-    int vat;
-    int quaReceipt
 }
