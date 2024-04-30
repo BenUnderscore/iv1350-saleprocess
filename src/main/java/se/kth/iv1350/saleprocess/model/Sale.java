6c41bab1240b7c232ce728c95037d4b14f22d1ca
Vladimir Li <ainrave.ilsley@outlook.com>
2024-04-30 16:12:40 +0200

    accounting system + small fixes

--- src/main/java/se/kth/iv1350/saleprocess/model/Sale.java
+++ src/main/java/se/kth/iv1350/saleprocess/model/Sale.java
@@ -28,7 +28,6 @@ public class Sale {
         }
         newItem.quantity = itemQuantity;
         itemList.add(newItem);
-
     }
 
     /**
--- src/main/java/se/kth/iv1350/saleprocess/view/View.java
+++ src/main/java/se/kth/iv1350/saleprocess/view/View.java
@@ -2,7 +2,7 @@ package se.kth.iv1350.saleprocess.view;
 
 import se.kth.iv1350.saleprocess.controller.Controller;
 
-class View {
+public class View {
     private Controller controller;
 
     public View(Controller controller) {
