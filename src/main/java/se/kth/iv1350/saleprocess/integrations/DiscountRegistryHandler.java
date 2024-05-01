4c2b48b979f156faec86633c19a855293cb9abb6
Vladimir Li <ainrave.ilsley@outlook.com>
2024-05-01 17:40:44 +0200

    updated discount registry a little

--- src/main/java/se/kth/iv1350/saleprocess/integrations/DiscountRegistryHandler.java
+++ src/main/java/se/kth/iv1350/saleprocess/integrations/DiscountRegistryHandler.java
@@ -1,5 +1,15 @@
 package se.kth.iv1350.saleprocess.integrations;
 
+import java.util.ArrayList;
+
+import se.kth.iv1350.saleprocess.dto.ItemInfo;
+
 public class DiscountRegistryHandler {
-    
+    public DiscountRegistryHandler() {
+        
+    }
+
+    public int getDiscount(String customerID, int total, ArrayList<ItemInfo> itemList) {
+        return 0;
+    }
 }
