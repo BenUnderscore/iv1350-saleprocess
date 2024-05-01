c1f1a61ea8768e037f7c3697802da1e8f6ba0a2e
Vladimir Li <ainrave.ilsley@outlook.com>
2024-05-01 15:31:16 +0200

    View done

--- src/main/java/se/kth/iv1350/saleprocess/view/View.java
+++ src/main/java/se/kth/iv1350/saleprocess/view/View.java
@@ -1,6 +1,7 @@
 package se.kth.iv1350.saleprocess.view;
 
 import se.kth.iv1350.saleprocess.controller.Controller;
+import se.kth.iv1350.saleprocess.dto.RunningStatus;
 
 public class View {
     private Controller controller;
@@ -8,4 +9,22 @@ public class View {
     public View(Controller controller) {
         this.controller = controller;
     }
+
+    public void startSale(){
+        controller.startSale();
+    }
+
+    public void scanOneItem(String itemID){
+        RunningStatus status = controller.registerItems(itemID, 1);
+    }
+
+    public void endSale(){
+        int totalPrice = controller.endSale();
+    }
+
+    public void registerPayment(int amount){  
+        int change = controller.registerPayment(amount);
+    }
+
+
 }
