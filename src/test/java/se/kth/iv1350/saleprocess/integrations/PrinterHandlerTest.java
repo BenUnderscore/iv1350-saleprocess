6cce51fa98beb3684803f6772842faa6162f81b0
Benjamin <benjamin@zeroparallax.se>
2024-05-19 15:44:51 +0200

    Changed expected output for printer test

--- src/test/java/se/kth/iv1350/saleprocess/integrations/PrinterHandlerTest.java
+++ src/test/java/se/kth/iv1350/saleprocess/integrations/PrinterHandlerTest.java
@@ -59,9 +59,9 @@ public class PrinterHandlerTest {
         String[] correctResultLines = new String[] {
             "------------------ Begin receipt -------------------",
             "Time of Sale: 2024-04-30 15:29",
-            "BigWheel Oatmeal          4 x 29.90 SEK   119.60 SEK ",
+            "BigWheel Oatmeal          4 x 29.90 SEK 119.60 SEK ",
             "Nyckelmix 120g          420 x 69.00 SEK 28980.00 SEK ",
-            "Total:                                  26999.60 SEK",
+            "Total: 26999.60 SEK",
             "",
             "VAT: 3484.77 SEK",
             "",
